71c3c1031fadf44d376984f914a004af7be82e49
imihov <imihov@htmlbox.net>
2021-06-29 07:15:44 +0300

    Add search and category listing page base styles

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { HomePageComponent } from '../components/home-page/home-page.component';
 import { ListingComponent } from '../components/listing/listing.component';
+import { CategoriesComponent } from '../components/categories/categories.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'search', component: ListingComponent },
+  { path: 'categories', component: CategoriesComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, FormsModule  } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppComponent } from './components/app-component/app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { ListingComponent } from './components/listing/listing.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ListingComponent
+    ListingComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
